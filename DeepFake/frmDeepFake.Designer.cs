ac1645820465acb535deb9ffc44995b03cec7782
sarmar11 <sara.martinezginer@telefonica.com>
2021-08-14 19:01:25 +0200

    Create DeepFake POC repository

---
+++ DeepFake/frmDeepFake.Designer.cs
@@ -0,0 +1,353 @@
+﻿namespace DeepFake
+{
+    partial class frmDeepFake
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel = new System.Windows.Forms.Panel();
+            this.labelSelectNone = new System.Windows.Forms.Label();
+            this.labelInvalidURL = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.labelScore = new System.Windows.Forms.Label();
+            this.pictureBoxOrange = new System.Windows.Forms.PictureBox();
+            this.pictureBoxRed = new System.Windows.Forms.PictureBox();
+            this.pictureBoxGreen = new System.Windows.Forms.PictureBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.lblURL = new System.Windows.Forms.Label();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.panel.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOrange)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGreen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel.Controls.Add(this.labelSelectNone);
+            this.panel.Controls.Add(this.labelInvalidURL);
+            this.panel.Controls.Add(this.groupBox1);
+            this.panel.Controls.Add(this.checkBox3);
+            this.panel.Controls.Add(this.checkBox2);
+            this.panel.Controls.Add(this.checkBox1);
+            this.panel.Controls.Add(this.btnScan);
+            this.panel.Controls.Add(this.lblURL);
+            this.panel.Controls.Add(this.lblWelcome);
+            this.panel.Controls.Add(this.txtURL);
+            this.panel.Location = new System.Drawing.Point(8, 8);
+            this.panel.Margin = new System.Windows.Forms.Padding(2);
+            this.panel.MinimumSize = new System.Drawing.Size(557, 274);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(557, 274);
+            this.panel.TabIndex = 0;
+            // 
+            // labelSelectNone
+            // 
+            this.labelSelectNone.AutoSize = true;
+            this.labelSelectNone.ForeColor = System.Drawing.Color.Red;
+            this.labelSelectNone.Location = new System.Drawing.Point(48, 98);
+            this.labelSelectNone.Name = "labelSelectNone";
+            this.labelSelectNone.Size = new System.Drawing.Size(214, 13);
+            this.labelSelectNone.TabIndex = 9;
+            this.labelSelectNone.Text = "At least one API Service should be selected";
+            this.labelSelectNone.Visible = false;
+            // 
+            // labelInvalidURL
+            // 
+            this.labelInvalidURL.AutoSize = true;
+            this.labelInvalidURL.ForeColor = System.Drawing.Color.Red;
+            this.labelInvalidURL.Location = new System.Drawing.Point(48, 79);
+            this.labelInvalidURL.Name = "labelInvalidURL";
+            this.labelInvalidURL.Size = new System.Drawing.Size(63, 13);
+            this.labelInvalidURL.TabIndex = 8;
+            this.labelInvalidURL.Text = "Invalid URL";
+            this.labelInvalidURL.Visible = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.Controls.Add(this.labelScore);
+            this.groupBox1.Controls.Add(this.pictureBoxOrange);
+            this.groupBox1.Controls.Add(this.pictureBoxRed);
+            this.groupBox1.Controls.Add(this.pictureBoxGreen);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(18, 130);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(524, 131);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "DeepFake Scan Results";
+            this.groupBox1.Visible = false;
+            // 
+            // labelScore
+            // 
+            this.labelScore.AutoSize = true;
+            this.labelScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelScore.Location = new System.Drawing.Point(434, 63);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(41, 13);
+            this.labelScore.TabIndex = 12;
+            this.labelScore.Text = "0/100";
+            // 
+            // pictureBoxOrange
+            // 
+            this.pictureBoxOrange.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxOrange.Image = global::DeepFake.Properties.Resources.orange;
+            this.pictureBoxOrange.Location = new System.Drawing.Point(405, 19);
+            this.pictureBoxOrange.MaximumSize = new System.Drawing.Size(102, 102);
+            this.pictureBoxOrange.MinimumSize = new System.Drawing.Size(102, 102);
+            this.pictureBoxOrange.Name = "pictureBoxOrange";
+            this.pictureBoxOrange.Size = new System.Drawing.Size(102, 102);
+            this.pictureBoxOrange.TabIndex = 11;
+            this.pictureBoxOrange.TabStop = false;
+            this.pictureBoxOrange.Visible = false;
+            // 
+            // pictureBoxRed
+            // 
+            this.pictureBoxRed.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxRed.Image = global::DeepFake.Properties.Resources.red;
+            this.pictureBoxRed.Location = new System.Drawing.Point(405, 19);
+            this.pictureBoxRed.MaximumSize = new System.Drawing.Size(102, 102);
+            this.pictureBoxRed.MinimumSize = new System.Drawing.Size(102, 102);
+            this.pictureBoxRed.Name = "pictureBoxRed";
+            this.pictureBoxRed.Size = new System.Drawing.Size(102, 102);
+            this.pictureBoxRed.TabIndex = 9;
+            this.pictureBoxRed.TabStop = false;
+            this.pictureBoxRed.Visible = false;
+            // 
+            // pictureBoxGreen
+            // 
+            this.pictureBoxGreen.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxGreen.Image = global::DeepFake.Properties.Resources.green;
+            this.pictureBoxGreen.Location = new System.Drawing.Point(405, 19);
+            this.pictureBoxGreen.MaximumSize = new System.Drawing.Size(102, 102);
+            this.pictureBoxGreen.MinimumSize = new System.Drawing.Size(102, 102);
+            this.pictureBoxGreen.Name = "pictureBoxGreen";
+            this.pictureBoxGreen.Size = new System.Drawing.Size(102, 102);
+            this.pictureBoxGreen.TabIndex = 7;
+            this.pictureBoxGreen.TabStop = false;
+            this.pictureBoxGreen.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(76, 87);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "ResultAPI3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(76, 64);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "ResultAPI2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(76, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "ResultAPI1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "FakeAPI3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "FakeAPI2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "FakeAPI1";
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(443, 98);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(73, 17);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "FakeAPI3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(443, 75);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(73, 17);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "FakeAPI2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(443, 52);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(73, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "FakeAPI1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // btnScan
+            // 
+            this.btnScan.Location = new System.Drawing.Point(341, 94);
+            this.btnScan.Margin = new System.Windows.Forms.Padding(2);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(81, 21);
+            this.btnScan.TabIndex = 3;
+            this.btnScan.Text = "Scan";
+            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
+            // 
+            // lblURL
+            // 
+            this.lblURL.AutoSize = true;
+            this.lblURL.Location = new System.Drawing.Point(15, 56);
+            this.lblURL.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblURL.Name = "lblURL";
+            this.lblURL.Size = new System.Drawing.Size(32, 13);
+            this.lblURL.TabIndex = 2;
+            this.lblURL.Text = "URL:";
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Location = new System.Drawing.Point(15, 15);
+            this.lblWelcome.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(176, 13);
+            this.lblWelcome.TabIndex = 1;
+            this.lblWelcome.Text = "Welcome to Foca DeepFake Plugin";
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(51, 53);
+            this.txtURL.Margin = new System.Windows.Forms.Padding(2);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(371, 20);
+            this.txtURL.TabIndex = 0;
+            // 
+            // frmDeepFake
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 293);
+            this.Controls.Add(this.panel);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MinimumSize = new System.Drawing.Size(587, 332);
+            this.Name = "frmDeepFake";
+            this.Text = "Foca DeepFake Plugin";
+            this.panel.ResumeLayout(false);
+            this.panel.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOrange)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGreen)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        public System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Label lblURL;
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBoxGreen;
+        private System.Windows.Forms.Label labelScore;
+        private System.Windows.Forms.PictureBox pictureBoxOrange;
+        private System.Windows.Forms.PictureBox pictureBoxRed;
+        private System.Windows.Forms.Label labelSelectNone;
+        private System.Windows.Forms.Label labelInvalidURL;
+    }
+
+}
+
---
+++ DeepFake/frmDeepFake.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Windows.Forms;
+using PluginsAPI;
+
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+namespace DeepFake
+{
+    public partial class frmDeepFake : Form
+    {
+        public frmDeepFake()
+        {
+            InitializeComponent();
+        }
+        private void btnScan_Click(object sender, EventArgs e)
+        {
+            labelSelectNone.Visible = false;
+            labelInvalidURL.Visible = false;
+            groupBox1.Visible = false;
+            var uri = "";
+            try
+            {
+                var url = new Uri(txtURL.Text);
+                uri = url.AbsoluteUri;
+            }
+            catch
+            {
+                labelInvalidURL.Visible = true;
+            }
+            if (checkBox1.Checked | checkBox2.Checked | checkBox3.Checked)
+            {
+                if (!labelInvalidURL.Visible) {
+                    _ = SendUriToScanAsync((object)uri);
+                }
+            }
+            else {
+                labelSelectNone.Visible = true;
+            }  
+        }
+
+
+        /// <summary>
+        /// Send value to FakeAPI and analize the results.
+        /// Generic code to extract data from API
+        /// var client = new HttpClient();
+        /// string baseURL = "https://fake1.api";
+        /// string videoURL = o.ToString();
+        /// string urlReport = "/fake/request?video-url=";
+        /// string accessToken = "fakeAccessToken1";
+        /// client.DefaultRequestHeaders.Add("Authorization", "Bearer " + accessToken);
+        /// var response = await client.GetStringAsync(baseURL + urlReport + videoURL);
+        /// JObject json = JObject.Parse(response);
+        /// var dfDetected = json["results"]["detected"];
+        /// var dfScore = json["results"]["score"];
+        /// </summary>
+        /// <param name="value"></param>
+        private async Task SendUriToScanAsync(object o)
+        {
+            int dfScore = 0;
+            int nScore = 0;
+            label4.Text = "Not Analyzed";
+            label5.Text = "Not Analyzed";
+            label6.Text = "Not Analyzed";
+
+            if (checkBox1.Checked)
+            {
+                // DeefFakeAPI1 Analysis
+                // Include code to extract data from API1
+                bool dfDetected = true;
+                dfScore += int.Parse("90");
+                nScore += 1;
+                if (dfDetected)
+                {
+                    label4.Text = "The video analyzed is classified as DEEPFAKE ";
+                }
+                else
+                {
+                    label4.Text = "The video analyzed is NOT classified as DEEPFAKE";
+                }
+            }
+            if (checkBox2.Checked)
+            {
+                // DeefFakeAPI2 Analysis
+                // Include code to extract data from API2
+                bool dfDetected = false;
+                dfScore += int.Parse("5");
+                nScore += 1;
+                if (dfDetected)
+                {
+                    label5.Text = "The video analyzed is classified as DEEPFAKE";
+                }
+                else
+                {
+                    label5.Text = "The video analyzed is NOT classified as DEEPFAKE";
+                }
+            }
+            if (checkBox3.Checked)
+            {
+                // DeefFakeAPI3 Analysis
+                // Include code to extract data from API3
+                bool dfDetected = true;
+                dfScore += int.Parse("55");
+                nScore += 1;
+                if (dfDetected)
+                {
+                    label6.Text = "The video analyzed is classified as DEEPFAKE ";
+                }
+                else
+                {
+                    label6.Text = "The video analyzed is NOT classified as DEEPFAKE";
+                }
+            }
+            dfScore /= nScore;
+            groupBox1.Visible = true;
+            labelScore.Text = dfScore.ToString() + "/100";
+            if (dfScore < 33)
+            {
+                pictureBoxGreen.Visible = true;
+                pictureBoxOrange.Visible = false;
+                pictureBoxRed.Visible = false;
+            }
+            if (33 <= dfScore && 66 > dfScore)
+            {
+                pictureBoxGreen.Visible = false;
+                pictureBoxOrange.Visible = true;
+                pictureBoxRed.Visible = false;
+            }
+            if (dfScore >= 66)
+            {
+                pictureBoxGreen.Visible = false;
+                pictureBoxOrange.Visible = false;
+                pictureBoxRed.Visible = true;
+            }
+        }
+    }
+}
